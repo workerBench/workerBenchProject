fbad35198728e7225e4cd1cc60753f7efc11a513
unknown <dongjooyoung@naver.com>
2023-03-06 21:46:14 +0900

    Test: test형식 useClass=>useValue로 변경, 워크숍 승인 및 반려 API TEST코드 작성 완료

--- src/admin/admin.service.spec.ts
+++ src/admin/admin.service.spec.ts
@@ -3,10 +3,21 @@ import { AdminService } from './admin.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { WorkShop } from '../entities/workshop';
+import { NotFoundException } from '@nestjs/common';
+
+// 사용할 메소드들 jest.fn()으로 가상 사용
+const mockAdminRepository = () => ({
+  find: jest.fn(),
+  findOne: jest.fn(),
+  update: jest.fn()
+})
+
+//Mock 타입 주입
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('AdminService', () => {
   let adminService: AdminService;
-  let workshopRepository: Repository<WorkShop>;
+  let workShopRepository: MockRepository<WorkShop>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,40 +25,119 @@ describe('AdminService', () => {
         AdminService,
         {
           provide: getRepositoryToken(WorkShop),
-          useClass: Repository,
+          useValue: mockAdminRepository(),
         },
       ],
     }).compile();
 
     adminService = module.get<AdminService>(AdminService);
-    workshopRepository = module.get<Repository<WorkShop>>(getRepositoryToken(WorkShop));
+    workShopRepository = module.get<MockRepository<WorkShop>>(getRepositoryToken(WorkShop));
   });
 
   // 검토 대기중인 워크숍 목록 TEST
   describe('requestWorkshops', () => {
     it('should return a list of requested workshops', async () => {
-      const workshop1 = new WorkShop();
-      workshop1.id = 1;
-      workshop1.status = 'request';
 
-      const workshop2 = new WorkShop();
-      workshop1.id = 2;
-      workshop1.status = 'approval';
+      const workShops = [{
+        id: 1,
+        name: '워크숍 이름',
+        status: 'request'
+      }, 
+      {
+        id: 2,
+        name: '워크숍2 이름',
+        status: 'request'
+      }]
 
-      jest.spyOn(workshopRepository, 'find').mockResolvedValueOnce([workshop1]);
+      jest.spyOn(workShopRepository, 'find').mockResolvedValueOnce([workShops]);
 
       const result = await adminService.requestWorkshops();
 
-      expect(workshopRepository.find).toHaveBeenCalledWith({
+      expect(workShopRepository.find).toHaveBeenCalledWith({
         where: { status: 'request' },
       });
-      expect(result).toEqual([workshop1]);
+      expect(result).toEqual([workShops]);
     });
   });
 
-  // describe('approveWrokshop', () => {
-  //   it('should update workshop status approval', async () => {
-      
-  //   })
-  // })
+  // 워크숍 승인하기 TEST
+  describe('approveWorkshop', () => {
+    it('should approve a workshop', async () => {
+      const mockWorkshop = {
+        id: 1,
+        name: '워크숍 이름',
+        status: 'request',
+      };
+      jest.spyOn(workShopRepository, 'findOne').mockResolvedValueOnce(mockWorkshop);
+      jest.spyOn(workShopRepository, 'update').mockResolvedValueOnce(undefined);
+
+      const result = await adminService.approveWorkshop(mockWorkshop.id);
+
+      expect(workShopRepository.findOne).toHaveBeenCalledWith({
+        where: { id: mockWorkshop.id, status: 'request' },
+      });
+      expect(workShopRepository.update).toHaveBeenCalledWith(mockWorkshop.id, { status: 'approval' });
+      expect(result).toEqual(undefined);
+    });
+
+    it('should throw a NotFoundException if the workshop does not exist or has already been approved', async () => {
+      const mockWorkshop = {
+        id: 1,
+        name: 'Workshop 1',
+        status: 'approval',
+      };
+      jest.spyOn(workShopRepository, 'findOne').mockResolvedValueOnce(undefined);
+
+      await expect(adminService.approveWorkshop(mockWorkshop.id)).rejects.toThrowError(
+        new NotFoundException('없는 워크숍입니다.'),
+      );
+
+      jest.spyOn(workShopRepository, 'findOne').mockResolvedValueOnce(mockWorkshop);
+
+      await expect(adminService.approveWorkshop(mockWorkshop.id)).rejects.toThrowError(
+        new NotFoundException('없는 워크숍입니다.'),
+      );
+    });
+  });
+
+  // 워크숍 반려하기 TEST
+  describe('rejectWorkshop', () => {
+    it('should reject a workshop', async () => {
+      const mockWorkshop = {
+        id: 1,
+        name: '워크숍 이름',
+        status: 'request',
+      };
+      jest.spyOn(workShopRepository, 'findOne').mockResolvedValueOnce(mockWorkshop);
+      jest.spyOn(workShopRepository, 'update').mockResolvedValueOnce(undefined);
+
+      const result = await adminService.rejectWorkshop(mockWorkshop.id);
+
+      expect(workShopRepository.findOne).toHaveBeenCalledWith({
+        where: { id: mockWorkshop.id, status: 'request' },
+      });
+      expect(workShopRepository.update).toHaveBeenCalledWith(mockWorkshop.id, { status: 'rejected' });
+      expect(result).toEqual(undefined);
+    });
+
+    it('should throw a NotFoundException if the workshop does not exist or has already been rejected', async () => {
+      const mockWorkshop = {
+        id: 1,
+        name: 'Workshop 1',
+        status: 'rejected',
+      };
+      jest.spyOn(workShopRepository, 'findOne').mockResolvedValueOnce(undefined);
+
+      await expect(adminService.approveWorkshop(mockWorkshop.id)).rejects.toThrowError(
+        new NotFoundException('없는 워크숍입니다.'),
+      );
+
+      jest.spyOn(workShopRepository, 'findOne').mockResolvedValueOnce(mockWorkshop);
+
+      await expect(adminService.approveWorkshop(mockWorkshop.id)).rejects.toThrowError(
+        new NotFoundException('없는 워크숍입니다.'),
+      );
+    });
+  });
+
 });
