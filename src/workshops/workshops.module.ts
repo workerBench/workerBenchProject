e3597b867eb085e99d31315027a79fcd39f402d8
minzunim <greenteapig6@gmail.com>
2023-03-07 17:56:17 +0900

    Fix: workshop.repository.ts 이름 수정

--- src/workshops/workshops.module.ts
+++ src/workshops/workshops.module.ts
@@ -4,7 +4,7 @@ import { Order } from 'src/entities/order';
 import { WishList } from 'src/entities/wish-list';
 import { WorkShop } from 'src/entities/workshop';
 import { WorkShopInstanceDetail } from 'src/entities/workshop-instance.detail';
-import { WorkshopRepository } from 'src/workshops/order.repository';
+import { WorkshopRepository } from 'src/workshops/workshop.repository';
 import { WorkshopsController } from './workshops.controller';
 import { WorkshopsControllerRender } from './workshops.render.controller';
 import { WorkshopsService } from './workshops.service';
--- src/workshops/workshops.service.ts
+++ src/workshops/workshops.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from 'src/entities/order';
 import { WishList } from 'src/entities/wish-list';
 import { WorkShop } from 'src/entities/workshop';
-import { WorkshopRepository } from 'src/workshops/order.repository';
+import { WorkshopRepository } from 'src/workshops/workshop.repository';
 import { Repository } from 'typeorm';
 
 @Injectable()
