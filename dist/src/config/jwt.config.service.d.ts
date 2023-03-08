1b0f316447ac097072f12a0087301b6bff7f9064
unknown <dongjooyoung@naver.com>
2023-03-08 17:38:18 +0900

    Refactor: 워크숍 검색 API 추가 innerJoin 및 필요한 정보만 불러오는 로직 코드 작성

--- dist/src/config/jwt.config.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
-export declare class JwtConfigService implements JwtOptionsFactory {
-    private readonly configService;
-    constructor(configService: ConfigService);
-    createJwtOptions(): JwtModuleOptions;
-}
--- dist/src/entities/genre-tag.d.ts
+++
@@ -1,9 +0,0 @@
-import { WorkShop } from './workshop';
-export declare class GenreTag {
-    id: number;
-    name: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deletedAt: Date | null;
-    WorkShopList: WorkShop[];
-}
