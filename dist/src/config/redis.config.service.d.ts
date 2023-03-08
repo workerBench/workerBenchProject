112fe750c639a1fe709ea8190334d84536e7bae0
msdou46 <msdou46@gmail.com>
2023-03-08 14:23:32 +0900

    Dosc seed 용 데이터- purpose_tag, genre_tag 수정

--- dist/src/config/redis.config.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { RedisOptionsFactory, RedisModuleOptions } from '@liaoliaots/nestjs-redis';
-import { ConfigService } from '@nestjs/config';
-export declare class RedisConfigService implements RedisOptionsFactory {
-    private configService;
-    constructor(configService: ConfigService);
-    createRedisOptions(): Promise<RedisModuleOptions>;
-}
