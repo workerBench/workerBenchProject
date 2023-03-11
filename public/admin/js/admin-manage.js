89138a469e7f7bfecc824ff68b2049c54d28c971
unknown <dongjooyoung@naver.com>
2023-03-12 02:31:30 +0900

    Design: 기존의 sidebar 부분을 더욱 상세하게 나눴고, 그에따라 렌더페이지 증가함. 추가로 필요한 API 작성 후 붙일 예정 (비슷한 기능들이라 금방 만들듯)

--- public/admin/js/admin-manage.js
+++ public/admin/js/admin-manage.js
@@ -1,7 +1,6 @@
 axios.get("../api/admin/admin/list")
   .then(function(response) {
     let adminUsers = response.data;
-    console.log(adminUsers);
     let html = '';
     for (let adminUser of adminUsers) {
       html += `
@@ -9,7 +8,7 @@ axios.get("../api/admin/admin/list")
           <td>${adminUser.id}</td>
           <td>${adminUser.name}</td>
           <td>${adminUser.email}</td>
-          <td>${adminUser.createdAt}</td>
+          <td>${adminUser.createdAt.split('T')[0]}</td>
           <td>${adminUser.admin_type === 0 ? 'normal admin' : ''}</td>
           <td>
             <button class="admin-remove-btn">계정 삭제</button>
