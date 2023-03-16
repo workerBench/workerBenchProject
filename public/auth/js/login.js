3eea9c0f9876446be93d10310611dc8fdae9d0e2
msdou46 <msdou46@gmail.com>
2023-03-16 14:38:51 +0900

    Feat access 토큰 에러 핸들링 작업중 1

---
+++ public/auth/js/login.js
@@ -0,0 +1,33 @@
+// 로그인 시도
+const login = async () => {
+  const email = document.querySelector('#inputEmail').value;
+  const password = document.querySelector('#inputPassword').value;
+
+  if (!email || !email.includes('@') || !email.includes('.')) {
+    alert('이메일을 정확히 입력해 주세요');
+    return;
+  }
+  if (!password) {
+    alert('비밀번호를 입력해 주세요');
+    return;
+  }
+
+  try {
+    const res2 = await axios({
+      method: 'POST',
+      url: '/api/auth/login/user',
+      data: { email, password },
+    });
+    // location.href = '/';
+    const beforeUrl = document.referrer; // 이전에 유입된 주소.
+    if (beforeUrl.includes('password/change')) {
+      location.href = '/';
+    } else {
+      location.href = beforeUrl;
+    }
+  } catch (err) {
+    alert(
+      `에러 코드: ${err.response.data.statusCode} / message: ${err.response.data.message}`,
+    );
+  }
+};
