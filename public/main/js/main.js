04b5f6fa6180a4ea49ca235f3d99624d5fce415f
minzunim <greenteapig6@gmail.com>
2023-03-22 17:28:56 +0900

    Design: 워크샵 검색 디자인 수정 + 메인 배너 이미지 css 수정

--- public/main/js/main.js
+++ public/main/js/main.js
@@ -22,11 +22,11 @@ function getBestWorkshops() {
             <span class="workshop-category">${
               element.workshop_category === 'online' ? '온라인' : '오프라인'
             }</span>
-            <p class="workshop-price">비용: ${element.workshop_price}원</p>
-            <p class="card-text">인원: ${element.workshop_min_member}~${
+            <p class="workshop-price">비용 &nbsp;${element.workshop_price}원</p>
+            <p class="card-text">인원 &nbsp;${element.workshop_min_member}~${
           element.workshop_max_member
         }명</p>
-            <p class="card-text">시간: ${element.workshop_total_time}분</p>
+            <p class="card-text">시간 &nbsp;${element.workshop_total_time}분</p>
             <p class="card-text">#${element.genre_tag_name} #${
           element.purpose_name[0]
         } #${element.purpose_name[1]}</p>
@@ -60,11 +60,11 @@ function getNewWorkshops() {
           <span class="workshop-category">${
             element.workshop_category === 'online' ? '온라인' : '오프라인'
           }</span>
-          <p class="workshop-price">비용: ${element.workshop_price}원</p>
-          <p class="card-text">인원: ${element.workshop_min_member}~${
+          <p class="workshop-price">비용 &nbsp;${element.workshop_price}원</p>
+          <p class="card-text">인원 &nbsp;${element.workshop_min_member}~${
           element.workshop_max_member
         }명</p>
-          <p class="card-text">시간: ${element.workshop_total_time}분</p>
+          <p class="card-text">시간 &nbsp;${element.workshop_total_time}분</p>
           <p class="card-text">#${element.genre_tag_name} #${
           element.purpose_name[0]
         } #${element.purpose_name[1]}</p>
